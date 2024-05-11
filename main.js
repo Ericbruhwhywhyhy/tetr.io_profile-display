caa02fc97b9487025492e07317a451c5b26ec7be
Ericbruhwhywhyhy <ericdo0169@gmail.com>
2024-05-11 18:15:22 +0700

    update

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 //tetra channel api wirh cors proxy cause otherwise this wouldn't work
-const URL = "https://api.codetabs.com/v1/proxy/?quest=http://ch.tetr.io/api/users/ericbruhwhywhyhy"
+const tetrioname = document.getElementById("name");
+const URL = 'https://api.codetabs.com/v1/proxy?quest=https://ch.tetr.io/api/users/';
 
-async function tetriodata() {
-  const response = await fetch(URL);
+async function plzfetch() {
+  console.log(tetrioname)
+  const response = await fetch(URL + tetrioname.value);
   const tetriostats = await response.json();
-//log the username of the Profile :oyes:
-  console.log(tetriostats.data.user.username)
-}
-
-tetriodata();
+  //log the username of the Profile :oyes:
+  console.log(JSON.stringify(tetriostats))
+  document.getElementById("rank").innerHTML = JSON.stringify(tetriostats.data.user.league.rank)
+  //yeah this doesn't work lol
+  if (JSON.stringify(tetriostats.data.user.league.rank) === "z") {
+    document.getElementById("rankimg").innerHTML = "https://tetr.io/res/league-ranks/z.png"
+  }
+};
